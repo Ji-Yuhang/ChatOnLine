feb458b108c87d51d345141c86b924b0389bd316
Ji-Yuhang <yuhang.silence@gmail.com>
2015-04-10 02:11:14 +0800

    init

---
+++ ChatOnlineServer/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "server.h"
+using namespace std;
+
+int main()
+{
+    cout << "This Chat On Line Server!" << endl;
+    cout << "wait new client..." << endl;
+    Server server;
+    return 0;
+}
+
